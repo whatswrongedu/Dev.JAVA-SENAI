102fdc0ef46ec0203db919d4e672c7c043217604
whatswrongedu <eduardoscruz1105@gmail.com>
2025-08-05 17:28:01 -0300

    ifElsePart4

---
+++ javaConsole/07.ifElsePart4/src/com/ifElse/app/App.java
@@ -0,0 +1,22 @@
+package com.ifElse.app;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner leia = new Scanner(System.in);
+
+        String nome;
+        int idade;
+
+        System.out.println("Informe seu nome: ");
+        nome = leia.nextLine();
+        System.out.println("Informe sua idade: ");
+        idade = leia.nextInt();
+
+        // operador ternário
+        System.out.println(nome + ((idade >= 18)? " é maior de idade." : " é menor de dade."));
+
+        leia.close();
+    }
+}
