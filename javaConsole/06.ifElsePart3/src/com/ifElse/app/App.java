ceca75f7a789dda8fbfe4d83f4eabcd618c9c6d6
whatswrongedu <eduardoscruz1105@gmail.com>
2025-08-05 16:43:57 -0300

    ifElsePart3

--- javaConsole/06.ifElsePart3/src/com/ifElse/app/App.java
+++ javaConsole/06.ifElsePart3/src/com/ifElse/app/App.java
@@ -6,7 +6,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner leia = new Scanner(System.in);
 
+        String nome;
+        double nota;
 
+        System.out.println("Informe o nome do aluno(a): ");
+        nome = leia.nextLine();
+        System.out.println("Informe a nota do aluno(a): ");
+        nota = leia.nextDouble();
+
+        if(nota >= 7){
+            System.out.println(nome + " está aprovado.");
+        }else if (nota >= 5){
+            System.out.println(nome + " está de recuperação.");
+        }else{
+            System.out.println(nome + " está reprovado.");
+        }
 
         leia.close();
     }
