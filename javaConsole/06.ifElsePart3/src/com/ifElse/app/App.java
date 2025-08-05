a3c72761e446f030967f0589257bbdbcc5f33149
whatswrongedu <eduardoscruz1105@gmail.com>
2025-08-05 16:20:27 -0300

    push de emergência

---
+++ javaConsole/06.ifElsePart3/src/com/ifElse/app/App.java
@@ -0,0 +1,13 @@
+package com.ifElse.app;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner leia = new Scanner(System.in);
+
+
+
+        leia.close();
+    }
+}
